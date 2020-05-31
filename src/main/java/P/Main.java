2f3d9440b70b4b730ec6f7c85d2ee29a5a4a1d49
L0615T1C5-216AC-9437 <60281819+L0615T1C5-216AC-9437@users.noreply.github.com>
2020-05-31 00:03:20 -0400

    Initial commit

---
+++ src/main/java/P/Main.java
@@ -0,0 +1,29 @@
+package P;
+
+import arc.util.CommandHandler;
+import mindustry.Vars;
+import mindustry.entities.type.Player;
+import mindustry.plugin.Plugin;
+
+public class Main extends Plugin {
+    //Var
+    ///Var
+    //on start
+    public Main() {
+    }
+
+    public void registerClientCommands(CommandHandler handler) {
+        handler.<Player>register("async", "toggle auto-sync", (arg, player) -> {
+            StringBuilder builder = new StringBuilder();
+            builder.append("[accent]List of all Players:");
+            for (Player p : Vars.playerGroup.all()) {
+                if (p.isAdmin) {
+                    builder.append("[gold]\uE828 [white]: ");
+                }
+                builder.append("[accent]#").append(p.id).append(" [white]: ").append("[#").append(p.color).append("]").append(p.name.replaceAll("\\[", "[[")).append("\n");
+            }
+            player.sendMessage(builder.toString());
+        });
+    }
+
+}
